5adcabb8ed8541b569e3c0d85de238f2dd6bc091
Soul Food Cafe Dev <mysoulfoodcafe@gmail.com>
2025-02-24 07:56:50 +0000

    using bind volume

--- Config/SoulFoodReportConfig.cs
+++ Config/SoulFoodReportConfig.cs
@@ -1,20 +1,23 @@
 using System.Configuration;
 using soulFoodReport.Persistency;
 
-public interface ISoulFoodReportConfig {
-    string PersistencyMainFolder {get;}
-    string[] Supplies {get;}
-    string[] ExpensesTypes {get;}
+public interface ISoulFoodReportConfig
+{
+    string PersistencyMainFolder { get; }
+    string[] Supplies { get; }
+    string[] ExpensesTypes { get; }
 }
-public class SoulFoodReportConfig {
+public class SoulFoodReportConfig
+{
     public static ISoulFoodReportConfig Instance = new DefaultSoulFoodReportConfig();
-    public static void SetConfigurationManger(ConfigurationManager configuration) {
+    public static void SetConfigurationManger(ConfigurationManager configuration)
+    {
         _configurationManager = configuration;
     }
     private static ConfigurationManager? _configurationManager;
-    private class DefaultSoulFoodReportConfig : ISoulFoodReportConfig
+    private sealed class DefaultSoulFoodReportConfig : ISoulFoodReportConfig
     {
-        public string PersistencyMainFolder => _configurationManager?.GetValue<string>("PersistencyMainFolder") ?? "";
+        public string PersistencyMainFolder { get; } = "/data/soulFoodMovs"; // default volume target
         public string[] Supplies => ExpensesConfigPersistency.LoadSupplies().ToArray();
         public string[] ExpensesTypes => ExpensesConfigPersistency.LoadExpTypes().ToArray();
     }
